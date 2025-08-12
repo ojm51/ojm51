de9c47b191cae9cc023116f002fa58a9ccfba2f1
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2025-08-12 14:19:53 +0900

    Update readmeUpdate.js

--- readmeUpdate.js
+++ readmeUpdate.js
@@ -8,6 +8,7 @@ const staticContent = `
   <img src="https://capsule-render.vercel.app/api?type=rounded&color=gradient&height=180&text=Hello%20I'm%20Emma&animation=fadeIn&fontColor=ffffff&fontSize=70" />
 </div>
 <br>
+<br>
 
 <div align="center">
   <h2 style="border-bottom: 1px solid #d8dee4; color: #282d33;">
@@ -16,12 +17,12 @@ const staticContent = `
   <div style="margin: 0 auto; text-align: center;" align="center">
     <img src="https://img.shields.io/badge/Javascript-F7DF1E?style=for-the-badge&logo=Javascript&logoColor=white">
     <img src="https://img.shields.io/badge/React-61DAFB?style=for-the-badge&logo=React&logoColor=white">
-    <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=HTML5&logoColor=white">
-    <img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=CSS3&logoColor=white">
-    <br>
-    <br>
     <img src="https://img.shields.io/badge/Next.js-000000?style=for-the-badge&logo=Next.js&logoColor=white">
     <img src="https://img.shields.io/badge/typescript-%233178C6.svg?&style=for-the-badge&logo=typescript&logoColor=white" />
+    <!-- <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=HTML5&logoColor=white"> -->
+    <!-- <img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=CSS3&logoColor=white"> -->
+    <br>
+    <br>
     <img src="https://img.shields.io/badge/tailwind%20css-%2338B2AC.svg?&style=for-the-badge&logo=tailwind%20css&logoColor=white" />
     <img src="https://img.shields.io/badge/Prettier-F7B93E?style=for-the-badge&logo=Prettier&logoColor=white">
     <img src="https://img.shields.io/badge/Eslint-4B32C3?style=for-the-badge&logo=Eslint&logoColor=white">
@@ -35,6 +36,7 @@ const staticContent = `
   </div>
 </div>
 <br>
+<br>
 
 <div align="center">
   <h2 style="border-bottom: 1px solid #d8dee4; color: #282d33;">
@@ -47,13 +49,6 @@ const staticContent = `
   </div>
 </div>
 <br>
-
-<div align="center">
-  <!-- Visitors -->
-  <a href="https://myhits.vercel.app">
-    <img src="https://myhits.vercel.app/api/hit/https%3A%2F%2Fgithub.com%2Fojm51?color=gray&label=👀&size=small" alt="👀" />
-  </a>
-</div>
 <br>
 
 <div align="center">
@@ -65,7 +60,14 @@ const staticContent = `
       <img src="https://img.shields.io/badge/Tistory-000000?style=for-the-badge&logo=Tistory&logoColor=white&link=https://dev-district.tistory.com">
     </a>
   </div>
+  <div align="center">
+    <!-- Visitors -->
+    <a href="https://myhits.vercel.app">
+      <img src="https://myhits.vercel.app/api/hit/https%3A%2F%2Fgithub.com%2Fojm51?color=gray&label=👀&size=small" alt="👀" />
+    </a>
+  </div>
 </div>
+<br>
 <br>
 
 <!-- <div align="center">
@@ -77,6 +79,7 @@ const staticContent = `
     <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=ojm51&layout=compact&bg_color=180,000000,&title_color=000000&text_color=000000" />
   </div>
 </div>
+<br>
 <br> -->
 `;
 
@@ -88,29 +91,33 @@ const parser = new Parser({
 });
 
 (async () => {
-  let blogSection = `
-  <div align="center">
-    <div style="display: inline-block; text-align: left;">
-  `;
+  let blogSection = "";
 
   try {
     // ✅ 아래 parseURL("") 안에 본인의 블로그 rss 페이지 주소를 입력
     const feed = await parser.parseURL("https://dev-district.tistory.com/rss");
     const latestPostsCount = 5; // 최신 글을 몇 개 가져올지
 
+    // 링크 HTML 생성
+    let linksHtml = "";
     for (let i = 0; i < latestPostsCount && i < feed.items.length; i++) {
       const { title, link } = feed.items[i];
-      blogSection += `<a href="${link}">${title}</a></br>\n`;
+      linksHtml += `<a href="${link}">${title}</a><br>\n`;
     }
+
+    blogSection = `
+    <div align="center">
+      <div style="display: inline-block; text-align: left;">
+        ${linksHtml}
+      </div>
+    </div>
+    `;
   } catch (error) {
     console.error("RSS 파싱 중 오류 발생:", error);
-    blogSection += "블로그 글을 불러오지 못했습니다.\n";
+    blogSection = `
+    <div align="center">블로그 글을 불러오지 못했습니다.</div>
+    `;
   }
-
-  blogSection += `
-    </div>
-  </div>
-  `;
 
   // ----- [3] 파일 작성 -----
   const finalContent = staticContent + blogSection;
